55f523b725dc80cf852ae46ec67c9fa7dfd6fe24
Chris White <chrisw@leehayes.com>
2018-04-08 16:15:01 -0400

    Removed loop test from XPath matcher

--- lib/XML/Axk/Matcher/XPath.pm
+++ lib/XML/Axk/Matcher/XPath.pm
@@ -42,9 +42,21 @@ sub test {
         say _dump $hrSAV if $VERBOSE>1;
     } if $VERBOSE>0;
 
-    my @matches = $hrSAV->{D}->findnodes($self->xpath);
-    say "XPath matches: ", _dump(\@matches) if $VERBOSE>0;
-    return any { $_ == $hrSAV->{E} } @matches;
+    # Works, but slow
+    #my @matches = $hrSAV->{D}->findnodes($self->xpath);
+    #say "XPath matches: ", _dump(\@matches) if $VERBOSE>0;
+    #return any { $_ == $hrSAV->{E} } @matches;
+
+    # Works.  We have to use the xp directly because XML::DOM::XPath v0.14 L59
+    # (https://metacpan.org/source/MIROD/XML-DOM-XPath-0.14/XPath.pm#L59)
+    # swaps the order of the document and node parameters in the call to
+    # xp->matches().
+    return $hrSAV->{D}->xp->matches($hrSAV->{E}, $self->xpath, $hrSAV->{D});
+        # Match {E} against path xpath, in context {D}.
+
+    #return $hrSAV->{D}->matches($hrSAV->{E}, $self->xpath);    # nope
+    #   XML::DOM::XPath doesn't provide a $context parameter for
+    #   Document->matches().
 
     #return $hrSAV->{E}->matches($self->xpath);
     #   didn't work - maybe becase we weren't starting from the root?
